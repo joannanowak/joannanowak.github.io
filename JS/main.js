14283b7c7ae68ae5b15a3386d38ef22efba469ab
Joanna Nowak <j.traciak@gmail.com>
2016-12-04 21:56:39 +0100

    zadanie1 link

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,25 @@
 // Podpowiedź: Zamień stringa na tablicę, wykonaj operacje odwracania kolejności i powróć do stringa * Sposobów rozwiązania tego zadania jest kilka, podpowiadam jedno z nich :)
 
 "use strict";
-var jakisstring = "Akademia108";
-var stringToArray = jakisstring.split("");
-console.log(stringToArray);
 
-var reversed = stringToArray.reverse();
-console.log(reversed);
+function odwroc(jakisstring) {
+	var jakisstring = "Akademia108";
+	var stringToArray = jakisstring.split(""); // dzieli string na tablice
+	console.log(stringToArray);
+
+	var reversed = stringToArray.reverse(); // odwraca elementy tablicy
+	console.log(reversed);
+
+	var reversed2 = reversed.reverse(); // odwraca odwroconą tablicę
+	console.log(reversed2);
+	
+	var noCommas = reversed2.join(" "); //łączy elementy tablicy bez przecinka
+	console.log(noCommas);
+	
+//	var joinedArrays = reversed2.toString(); //zmienia tablice w string z przecinkami
+//	console.log(joinedArrays);
+	
+}
+
+odwroc();
+
