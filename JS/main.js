d2ec45c6fc2e43fa019cdcb27fbb7d1318e882e7
Joanna Nowak <j.traciak@gmail.com>
2016-12-04 21:16:03 +0100

    js, index1, index

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,10 @@
+// Napisz funkcję, która pobiera jako parametr stringa (czyli napis) i zwraca ten sam napis tylko napisany wspak. Następnie odwróć wspak stringa "Akademia108" i wyświetl w konsoli. 
+// Podpowiedź: Zamień stringa na tablicę, wykonaj operacje odwracania kolejności i powróć do stringa * Sposobów rozwiązania tego zadania jest kilka, podpowiadam jedno z nich :)
+
 "use strict";
+var jakisstring = "Akademia108";
+var stringToArray = jakisstring.split("");
+console.log(stringToArray);
+
+var reversed = stringToArray.reverse();
+console.log(reversed);
