053b2f5f35d6b1a23c524f82c7df4a7375b74523
Joanna Nowak <j.traciak@gmail.com>
2016-12-05 00:40:52 +0100

    link update

--- JS/main.js
+++ JS/main.js
@@ -1,67 +1,64 @@
-//
-//// Napisz funkcję, która pobiera jako parametr stringa (czyli napis) i zwraca ten sam napis tylko napisany wspak. Następnie odwróć wspak stringa "Akademia108" i wyświetl w konsoli. 
-//// Podpowiedź: Zamień stringa na tablicę, wykonaj operacje odwracania kolejności i powróć do stringa * Sposobów rozwiązania tego zadania jest kilka, podpowiadam jedno z nich :)
-//
-//"use strict";
-//
-//var jakisstring;
-//
-//function odwroc() {
-//
-//	var stringToArray = jakisstring.split(""); // dzieli string na tablice
-//	console.log(stringToArray);
-//
-//	var reversed = stringToArray.reverse(); // odwraca elementy tablicy
-//	console.log(reversed);
-//
-//	var reversed2 = reversed.reverse(); // odwraca odwroconą tablicę
-//	console.log(reversed2);
-//
-//	var noCommas = reversed2.join(" "); //łączy elementy tablicy bez przecinka
-//	console.log(noCommas);
-//
-//	//	var joinedArrays = reversed2.toString(); //zmienia tablice w string z przecinkami
-//	//	console.log(joinedArrays);
-//
-//}
-//
-//odwroc("Akademia108");
-//
-//
-//
+// Napisz funkcję, która pobiera jako parametr stringa (czyli napis) i zwraca ten sam napis tylko napisany wspak. Następnie odwróć wspak stringa "Akademia108" i wyświetl w konsoli. 
+// Podpowiedź: Zamień stringa na tablicę, wykonaj operacje odwracania kolejności i powróć do stringa * Sposobów rozwiązania tego zadania jest kilka, podpowiadam jedno z nich :)
+
+"use strict";
+
+var jakisString;
+
+function odwroc() {
+
+	var stringToArray = jakisString.split(""); // dzieli string na tablice
+	console.log(stringToArray);
+
+	var reversed = stringToArray.reverse(); // odwraca elementy tablicy
+	console.log(reversed);
+
+	var reversed2 = reversed.reverse(); // odwraca odwroconą tablicę
+	console.log(reversed2);
+
+	var noCommas = reversed2.join(""); //łączy elementy tablicy bez przecinka
+	console.log(noCommas);
+
+	var joinedArrays = reversed2.toString(); //zmienia tablice w string z przecinkami
+	console.log(joinedArrays);
+
+}
+
+odwroc("Akademia108");
+
+
+
 
 
 //Zadanie w repl.it
-//// Napisz funkcję, która pobiera jako parametr stringa (czyli napis) i zwraca ten sam napis tylko napisany wspak. Następnie odwróć wspak stringa "Akademia108" i wyświetl w konsoli. 
-//// Podpowiedź: Zamień stringa na tablicę, wykonaj operacje odwracania kolejności i powróć do stringa * Sposobów rozwiązania tego zadania jest kilka, podpowiadam jedno z nich :)
-//
-//"use strict";
-//
-//var jakisstring;
-//
-//function odwroc(jakisstring) {
-//	
-//	var stringToArray = jakisstring.split(''); // dzieli string na tablice
-//	console.log(stringToArray);
-//
-//	var reversed = stringToArray.reverse(); // odwraca elementy tablicy
-//	console.log(reversed);
-//
-//	var reversed2 = reversed.reverse(); // odwraca odwroconą tablicę
-//	console.log(reversed2);
-//	
-//	var noCommas = reversed2.join(" "); //łączy elementy tablicy bez przecinka
-//	console.log(noCommas);
-//	
-//	var joinedArrays = reversed2.toString(); //zmienia tablice w string z przecinkami
-//	console.log(joinedArrays);
-//	
-//}
-//
-//odwroc("Akademia108");
-//
-//
-//
+// Napisz funkcję, która pobiera jako parametr stringa (czyli napis) i zwraca ten sam napis tylko napisany wspak. Następnie odwróć wspak stringa "Akademia108" i wyświetl w konsoli. 
+// Podpowiedź: Zamień stringa na tablicę, wykonaj operacje odwracania kolejności i powróć do stringa * Sposobów rozwiązania tego zadania jest kilka, podpowiadam jedno z nich :)
+
+
+var jakisDrugiString;
+
+function odwroc(jakisDrugiString) {
+
+	var stringToArray = jakisDrugiString.split(''); // dzieli string na tablice
+	console.log(stringToArray);
+
+	var reversed = stringToArray.reverse(); // odwraca elementy tablicy
+	console.log(reversed);
+
+	var reversed2 = reversed.reverse(); // odwraca odwroconą tablicę
+	console.log(reversed2);
+
+	var noCommas = reversed2.join(" "); //łączy elementy tablicy bez przecinka
+	console.log(noCommas);
+
+	var joinedArrays = reversed2.toString(); //zmienia tablice w string z przecinkami
+	console.log(joinedArrays);
+
+}
+
+odwroc("Akademia108");
+
+
 
 
 
@@ -73,15 +70,18 @@
 var tablica = [1, 2, 3, 4, 5, 6];
 
 function licz() {
-
 	var suma = 0;
-	for (var i = 0; i > tablica.length; ++i) {
+	for (var i = 0; i < tablica.length; ++i) {
 		suma += tablica[i];
 	}
+	console.log(suma);
 
-	return licz;
-	
+	var iloczyn = 1;
+	for (var i = 0; i < tablica.length; ++i) {
+		iloczyn *= tablica[i];
+	}
+	console.log(iloczyn);
+
 }
 
-licz(tablica);
-console.log(licz(tablica));
+licz();
