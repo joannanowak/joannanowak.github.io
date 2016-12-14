75a854a9e4c8173088051c89379ba784b915c1f1
Joanna Nowak <j.traciak@gmail.com>
2016-12-14 01:43:35 +0100

    added comments

--- JS/animations.js
+++ JS/animations.js
@@ -2,17 +2,18 @@ $(function(){
 var kwadrat = $('#kwadrat');
 var button = $('button');
 var napis = $('h2');
+var button2 = $('#button2');
 	
-	napis.hide();
-	button.click(function(){
-		kwadrat.animate({'margin-left':'100px'}, 1500, zmniejsz);
+	napis.hide(); //ukrywa napis h2
+	button.click(function(){ //po kliknięciu w button
+		kwadrat.animate({'margin-left':'100px'}, 1500, zmniejsz); //przesuń o 100px i zmniejsz
 		function zmniejsz(){
-			kwadrat.animate({'height': '100px', 'width': '100px'}, 1500, zmienKolor);
+			kwadrat.animate({'height': '100px', 'width': '100px'}, 1500, zmienKolor); //zmniejsz i zmien kolor
 			function zmienKolor(){
-				kwadrat.fadeOut(2500, function(){
-					kwadrat.css({'background-color': 'blue'}).fadeIn(2500, napisH2);
-					function napisH2(){
-						napis.show(2500);
+				kwadrat.fadeOut(2500, function(){ //ale najpierw zniknij fadeOut
+					kwadrat.css({'background-color': 'blue'}).fadeIn(2500, napisH2); //przypisz nowy kolor tla i pokaz napis
+					function napisH2(){ //pokaz napis
+						napis.show(2500); 
 					}
 				});
 			}
@@ -20,6 +21,9 @@ var napis = $('h2');
 	})
 	
 	
+	button2.click(function(){
+		
+	})
 	
 	
 	
